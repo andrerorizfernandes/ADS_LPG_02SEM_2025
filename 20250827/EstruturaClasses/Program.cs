8654c7aeff8bb48c81fb4781321cb3f23924623b
andrerorizfernandes <andremuriae@yahoo.com.br>
2025-08-27 20:05:29 -0300

    Adicionar arquivos de projeto.

---
+++ 20250827/EstruturaClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using EstruturaClasses;
+
+Veiculo v = new Veiculo();
+v.Placa = "ART2036";
+v.Potencia = 1.4;
+v.Montadora = "FIAT";
+v.Ano = 2025;
+var lValorLiquido = v.ValorLiquidoAluguel(5000, Veiculo.TipoPessoa.Juridica);
+
+string lMensagem =
+    $"Placa: {v.Placa}\n" +
+    $"Potência: {v.Potencia}\n" +
+    $"Montadora: {v.Montadora}\n" +
+    $"Ano: {v.Placa}\n\n" +
+    $"Valor líquido do aluguel: {lValorLiquido}";
+
+Console.WriteLine(lMensagem);
+Console.ReadKey();
---
+++ 20250827/EstruturaClasses/Veiculo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturaClasses
+{
+    public class Veiculo
+    {
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public string Modelo { get; set; }
+        public string Montadora { get; set; }
+        public int Ano { get; set; }
+        public double Potencia { get; set; }
+
+        public enum TipoPessoa
+        {
+            Fisica,
+            Juridica
+        }
+
+        public double ValorLiquidoAluguel(double pValorBruto, TipoPessoa pTipoPessoa)
+        {
+            const double DESCONTO_PESSOA_FISICA = 0.05;
+            const double DESCONTO_PESSOA_JURIDICA = 0.15;
+
+            var lValorDesconto = 0.0;
+            switch (pTipoPessoa)
+            {
+                case TipoPessoa.Fisica:
+                {
+                    lValorDesconto = (pValorBruto * DESCONTO_PESSOA_FISICA);
+                    break;
+                }
+                case TipoPessoa.Juridica:
+                    {
+                        lValorDesconto = (pValorBruto * DESCONTO_PESSOA_JURIDICA);
+                        break;
+                    }
+            }
+
+            return (pValorBruto - lValorDesconto);
+        }
+    }
+}
