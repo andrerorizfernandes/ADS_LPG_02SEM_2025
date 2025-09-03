38cb650546c048b5faa3976b2e1476682cd351ba
andrerorizfernandes <andremuriae@yahoo.com.br>
2025-09-02 21:54:27 -0300

    Aula do dia 02/09/2025

---
+++ 20250902/TesteClasses/Aluno.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteClasses
+{
+    public class Aluno
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+        public string NomePai { get; set; }
+        public string NomeMae { get; set; }
+        public string Escolaridade { get; set; }
+        public string Curso { get; set; }
+
+        public string Operacao(Aluno pAluno, AcaoAluno pAcaoAluno)
+        {
+            var lAcao = string.Empty;
+            switch (pAcaoAluno)
+            {
+                case AcaoAluno.Matricular:
+                    lAcao = "matriculado";
+                    break;
+                case AcaoAluno.Transferir:
+                    lAcao = "transferido";
+                    break;
+                case AcaoAluno.Evadir:
+                    lAcao = "evadido";
+                    break;
+            }
+
+            return $"O aluno de matrícula {pAluno.Matricula}, do curso {pAluno.Curso} foi {lAcao} \n\n" +
+              $"Dados completos do cadastro: \n" +
+              $"Matrícula: {pAluno.Matricula}, Nome: {pAluno.Nome}, Sexo: {pAluno.Sexo}, " +
+              $"Telefone: {pAluno.Telefone}, Endereço: {pAluno.Endereco}, Email: {pAluno.Email}, " +
+              $"Nome do Pai: {pAluno.NomePai}, Nome da Mãe: {pAluno.NomeMae}, " +
+              $"Escolaridade: {pAluno.Escolaridade}, Curso: {pAluno.Curso}";
+        }
+
+        public Aluno(string pCurso)
+        {
+            Matricula = 0;
+            Nome = string.Empty;
+            Sexo = string.Empty;
+            Telefone = string.Empty;
+            Endereco = string.Empty;
+            Email = string.Empty;
+            NomePai = string.Empty;
+            NomeMae = string.Empty;
+            Escolaridade = string.Empty;
+            Curso = pCurso;
+        }
+    }
+}
---
+++ 20250902/TesteClasses/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno("ADS");
+
+            Console.WriteLine("Informe a matrícula");
+            a.Matricula = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o nome");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o sexo");
+            a.Sexo = Console.ReadLine();
+            Console.WriteLine("Informe o telefone");
+            a.Telefone = Console.ReadLine();
+            Console.WriteLine("Informe o endereço");
+            a.Endereco = Console.ReadLine();
+            Console.WriteLine("Informe o email");
+            a.Email = Console.ReadLine();
+            Console.WriteLine("Informe o nome do pai");
+            a.NomePai = Console.ReadLine();
+            Console.WriteLine("Informe o nome da mãe");
+            a.NomeMae = Console.ReadLine();
+            Console.WriteLine("Informe a escolaridade");
+            a.Escolaridade = Console.ReadLine();
+            Console.WriteLine("Informe o curso");
+            var lCurso = Console.ReadLine();
+            if(lCurso.Trim() != string.Empty)
+            {
+                a.Curso = lCurso;
+            }
+
+            var lOperacao = AcaoAluno.Nenhum;
+            Console.WriteLine("Informe a operação: 1 - Matricular, 2 - Transferir, 3 - Evadir");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    lOperacao = AcaoAluno.Matricular;
+                    break;
+                case "2":
+                    lOperacao = AcaoAluno.Transferir;
+                    break;
+                case "3":
+                    lOperacao = AcaoAluno.Evadir;
+                    break;
+            }
+
+            var lTeste = a.Operacao(a, lOperacao);
+            Console.WriteLine(lTeste);
+            Console.ReadKey();
+        }
+    }
+}
