13b067524c5b844725c96b5601e2f8f4b38e0570
andrerorizfernandes <andremuriae@yahoo.com.br>
2025-10-07 21:55:53 -0300

    Aula do dia 07/10/2025

---
+++ Projetos_web/Projetos_web/Exemplo_02.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projetos_web
+{
+    public partial class Exemplo_02 : System.Web.UI.Page
+    {
+        private string MensagemFechamentoContrato(Boolean Aceito)
+        {
+            var lRetorno = string.Empty;
+
+            if (Aceito)
+            {
+                lRetorno = chkAceito.Text;
+            }
+
+            return lRetorno;
+        }
+
+        private string CodigoCorSelecionada()
+        {
+            return ddlDados.SelectedValue;
+        }
+
+        private void PreencherCores()
+        {
+            ddlDados.Items.Add(new ListItem("Amarelo", "100"));
+            ddlDados.Items.Add(new ListItem("Azul", "200"));
+            ddlDados.Items.Add(new ListItem("Verde", "300"));
+            ddlDados.Items.Add(new ListItem("Vermelho", "400"));
+            ddlDados.Items.Add(new ListItem("Rosa", "500"));
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) {
+                PreencherCores();
+            }
+        }
+
+        protected void chkAceito_CheckedChanged(object sender, EventArgs e)
+        {
+            lblInfo02.Text = MensagemFechamentoContrato(chkAceito.Checked);
+        }
+
+        protected void ddlDados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblInformacao.Text = CodigoCorSelecionada();
+        }
+    }
+}
---
+++ Projetos_web/Projetos_web/Exemplo_02.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Projetos_web
+{
+
+
+    public partial class Exemplo_02
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle lblInformacao.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInformacao;
+
+        /// <summary>
+        /// Controle lblInfo02.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInfo02;
+
+        /// <summary>
+        /// Controle chkAceito.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkAceito;
+
+        /// <summary>
+        /// Controle lbiCarros.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList lbiCarros;
+
+        /// <summary>
+        /// Controle lblInformacao3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInformacao3;
+
+        /// <summary>
+        /// Controle btnSelecionados.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSelecionados;
+
+        /// <summary>
+        /// Controle ddlDados.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlDados;
+    }
+}
