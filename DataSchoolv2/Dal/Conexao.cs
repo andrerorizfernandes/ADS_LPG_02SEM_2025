015a8bcf967410362783df1ca0de2a2488e3df20
andrerorizfernandes <andremuriae@yahoo.com.br>
2025-10-23 21:53:49 -0300

    Criando camandas de persistência

---
+++ DataSchoolv2/Dal/Conexao.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Conexao
+    {
+        private static string stringConexao = "server=127.0.0.1;user=root;password=;database=dataschool";
+
+        public static MySqlConnection ConectarBD()
+        {
+            MySqlConnection conexao = new MySqlConnection(stringConexao);
+            conexao.Open();
+            return conexao;
+        }       
+    }
+}
---
+++ DataSchoolv2/Dal/Persistencia.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using Pojo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Persistencia
+    {
+        public static MySqlDataReader ExecutarSQL(string pSQL, object pEntidade)
+        {
+            MySqlCommand comando = new MySqlCommand(pSQL, Conexao.ConectarBD());
+            if (pEntidade is Aluno aluno)
+            {
+
+            }
+        }
+    }
+}
