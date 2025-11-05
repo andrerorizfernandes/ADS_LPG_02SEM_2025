74a96e2e4455089c079394a420bb2e039b5dd8bc
andrerorizfernandes <andremuriae@yahoo.com.br>
2025-11-04 21:07:22 -0300

    Aula dia 04/11 - Camada BLL

--- DataSchoolv2/Bll/AlunoBll.cs
+++ DataSchoolv2/Bll/AlunoBll.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dal;
+using Pojo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,30 @@ namespace Bll
 {
     public class AlunoBll
     {
-        //continuar criando os métodos
+        private AlunoDal _alunoDal;
+
+        public AlunoBll()
+        {
+            _alunoDal = new AlunoDal();
+        }
+
+        public bool InserirAluno(Aluno aluno)
+        {
+            return _alunoDal.InserirAluno(aluno);
+        }
+        public bool EditarAluno(Aluno aluno)
+        {
+            return _alunoDal.EditarAluno(aluno);
+        }
+
+        public bool ExcluirAluno(Aluno aluno)
+        {
+            return _alunoDal.ExcluirAluno(aluno);
+        }
+
+        public List<Aluno> ListarAlunos()
+        {
+            return _alunoDal.ListarAlunos();
+        }
     }
 }
